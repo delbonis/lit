2501752f890bde86e6287fd8bcd8b9449ba5637d
Trey Del Bonis <j.delbonis.3@gmail.com>
2018-09-17 13:56:32 -0400

    Made gotests.sh return 1 if any go test invocation returns nonzero.

--- gotests.sh
+++ gotests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+fail=0
+bad=''
+
 for td in */; do
 	if [ "$td" == "build/" ]; then
 		continue
@@ -8,10 +11,20 @@ for td in */; do
 		continue
 	fi
 
-	gocnt=$(find $td -name '*.go' | wc -l)
+	gocnt=$(ls $td | grep -E '\.go$' | wc -l)
 	if [ $gocnt -gt 0 ]; then
 		echo "Running go test in $td"
 		go test -v ./$td
+		res=$?
+		if [ "$res" -ne "0" ]; then
+			fail=1
+			bad="$bad $td"
+		fi
 		echo ''
 	fi
 done
+
+if [ "$fail" -eq 1 ]; then
+	echo 'Failed:' $bad
+	exit 1
+fi
